e85e3c65d4c5eac7cf8d6b5348371ee41018f068
jaywcjlove <398188662@qq.com>
2015-05-28 15:06:23 +0800

    添加nodejs websocket 后端服务，提供测试

---
+++ test/sever.js
@@ -0,0 +1,77 @@
+var ws = require("nodejs-websocket");
+var server = ws.createServer(function(conn){
+    conn.on("text", function (str) {
+        console.log("收到的信息为:"+str)
+        conn.sendText(str)
+    })
+    conn.on("close", function (code, reason) {
+        console.log("关闭连接")
+    });
+    conn.on("error", function (code, reason) {
+        console.log("异常关闭")
+    });
+}).listen(3001)
+
+
+console.log("--WebSocket-------------")
+console.log("WebSocket address: ws://127.0.0.1:3001")
+console.log("WebSocket has started.")
+console.log("------------------------")
+
+
+var http = require("http"),
+    url  = require("url"),
+    path = require("path"),
+    fs   = require("fs");
+
+http.createServer(function (req, res) {
+    var pathname=__dirname+url.parse(req.url).pathname;
+    if (path.extname(pathname)=="") {
+        pathname+="/";
+    }
+    if (pathname.charAt(pathname.length-1)=="/"){
+        pathname+="index.html";
+    }
+
+    path.exists(pathname,function(exists){
+        if(exists){
+            switch(path.extname(pathname)){
+                case ".html":
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    break;
+                case ".js":
+                    res.writeHead(200, {"Content-Type": "text/javascript"});
+                    break;
+                case ".css":
+                    res.writeHead(200, {"Content-Type": "text/css"});
+                    break;
+                case ".gif":
+                    res.writeHead(200, {"Content-Type": "image/gif"});
+                    break;
+                case ".jpg":
+                    res.writeHead(200, {"Content-Type": "image/jpeg"});
+                    break;
+                case ".png":
+                    res.writeHead(200, {"Content-Type": "image/png"});
+                    break;
+                default:
+                    res.writeHead(200, {"Content-Type": "application/octet-stream"});
+            }
+
+            fs.readFile(pathname,function (err,data){
+                res.end(data);
+            });
+        } else {
+            res.writeHead(404, {"Content-Type": "text/html"});
+            res.end("<h1>404 Not Found</h1>");
+        }
+    });
+
+}).listen(8080, "127.0.0.1");
+
+
+console.log("--Server----------------")
+console.log("Server address: http://127.0.0.1:8080")
+console.log("Server running... press ctrl-c to stop.")
+console.log("Server has started.")
+console.log("------------------------")
